fc3f4319761cf212a8d70d0d54d67821ac25b9d7
SySon253 <162858665+SySon253@users.noreply.github.com>
2025-11-01 21:22:05 +0700

    Initial commit

---
+++ Store/Models/Cart.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Store.Models
+{
+    public class Cart
+    {
+        public List<CartLine> Lines { get; set; } = new List<CartLine>();
+        public void AddItem(Product product, int quantity)
+        {
+            var line = Lines.FirstOrDefault(p => p.ProductId == product.ProductId);
+            if (line == null)
+            {
+                Lines.Add(new CartLine
+                {
+                    ProductId = product.ProductId,
+                    ProductName = product.ProductName,
+                    ProductPrice = (decimal)product.ProductPrice,
+                    ProductDiscount = (decimal)product.ProductDiscount,
+                    ProductImage = product.ProductImage,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Product product) =>
+            Lines.RemoveAll(l => l.ProductId == product.ProductId);
+
+        public decimal ComputeTotalValues()
+        {
+            return Lines.Sum(e => e.ProductPrice * (1 - e.ProductDiscount) * e.Quantity);
+        }
+
+        public void Clear() => Lines.Clear();
+    }
+    public class CartLine
+    {
+        public int CartLineId { get; set; }
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public decimal ProductPrice { get; set; }
+        public decimal ProductDiscount { get; set; }
+        public string ProductImage { get; set; }
+
+        public int Quantity { get; set; }
+
+        // Không serialize Product để tránh vòng lặp khi lưu session
+        [JsonIgnore]
+        public Product Product { get; set; }
+    }
+}
---
+++ Store/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Store.Data;
+using Store.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("StoreContext");
+builder.Services.AddDbContext<StoreContext>(options =>
+    options.UseLazyLoadingProxies().UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+    options.EnableSensitiveDataLogging(); // <--- Dòng này giúp in SQL tham số
+    options.LogTo(Console.WriteLine, LogLevel.Information); // <--- Dòng này để in SQL ra console
+});
+
+
+builder.Services.AddScoped<UserService, UserServiceImpl>();
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+
+// Cấu hình Session
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+    options.Cookie.SameSite = SameSiteMode.Lax; 
+});
+
+// Cấu hình Authentication Cookie
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/User/Login";
+        options.LogoutPath = "/User/Logout";
+        options.AccessDeniedPath = "/User/Denied";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+        options.SlidingExpiration = true;
+        options.Cookie.Path = "/";
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SameSite = SameSiteMode.Lax; 
+    });
+builder.Services.AddAuthorization();
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
