6f7e716ccdf4ae066d095b760602a6968459551c
SySon253 <162858665+SySon253@users.noreply.github.com>
2025-11-07 22:02:41 +0700

    chinh sua cac file de hoan chinh

---
+++ Store/Models/ViewModels/PagingInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace Store.Models.ViewModels
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }      // tổng số sản phẩm
+        public int ItemsPerPage { get; set; }    // số sản phẩm trên mỗi trang
+        public int CurrentPage { get; set; }     // trang hiện tại
+
+        // Tính tổng số trang dựa trên 2 biến trên
+        public int TotalPages
+        {
+            get
+            {
+                if (ItemsPerPage == 0) return 0; // tránh chia cho 0
+                return (int)System.Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+            }
+        }
+    }
+}
